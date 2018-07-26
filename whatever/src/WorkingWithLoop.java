4dcb25a6d58264247ad0569cf5120fa400bd3345
somkiat <somkiat.p@gmail.com>
2018-07-26 11:36:47 +0700

    Loop

---
+++ whatever/src/WorkingWithLoop.java
@@ -0,0 +1,22 @@
+
+public class WorkingWithLoop {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+        
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+        i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }while(i < 5);
+
+    }
+
+}
